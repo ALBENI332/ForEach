48ed72d41361d46148bdd7a8eea2017f4c5c5d23
Benaya Aidarov <128293964+ALBENI332@users.noreply.github.com>
2024-10-30 19:25:17 +0600

    asdasd
    asd

---
+++ main.js
@@ -0,0 +1,114 @@
+// const num = [1, 2, 3] 
+ 
+// num.forEach((item) => { 
+//     console.log(item*2) 
+// }) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// const num = [1, 2, 3] 
+ 
+// let sum = 0 
+ 
+// num.forEach((item) => { 
+//     console.log(sum+=item) 
+// }) 
+ 
+ 
+ 
+ 
+ 
+ 
+// const word = [2, 3, 4, 5, 6, 7, 8] 
+ 
+// word.forEach((item) => { 
+//     if(item > 5){ 
+//         console.log(item) 
+//     } 
+// }) 
+ 
+ 
+ 
+ 
+// const num = [1, 2, 3, 4, 5, 6] 
+ 
+// const filterArr = num.filter((item) => { 
+//     if (item % 2==0){ 
+//         return true 
+//     } 
+// }) 
+ 
+// console.log(filterArr) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// const animals = ['kot', 'cobaka', 'tigr', 'lev'] 
+ 
+// const filterAnimal = animals.filter((item) => { 
+//     if(item.length >3){ 
+//         return true 
+//     } 
+// }) 
+ 
+// console.log(filterAnimal) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// const num = [1, 20, 30, 5, 6, 11, 10000000000] 
+// const filternum = num.filter((item) => { 
+//     if (item > 10){ 
+//         console.log(item) 
+//     } 
+// }) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// const fruits = ['апельсин', 'яблоко', 'арбуз', 'банан']; 
+ 
+// const filteredFruits = fruits.filter(fruit => fruit.toLowerCase().startsWith('а')); 
+ 
+// console.log(filteredFruits);  
+ 
+ 
+ 
+ 
+ 
+ 
+// const users = [ 
+//     { name: 'Alice', age: 22 }, 
+//     { name: 'Bob', age: 17 }, 
+//     { name: 'Charlie', age: 19 }, 
+//     { name: 'David', age: 15 }, 
+//     { name: 'Eve', age: 25 } 
+// ]; 
+ 
+// const adults = users.filter(user => user.age > 18); 
+ 
+// console.log(adults);
